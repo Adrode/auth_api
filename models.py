a69e63e51b6c313f9b1794b69f5373854f5f524a
Adrian Wozniak <adrianwozniak20@gmail.com>
2026-03-23 19:26:27 +0100

    fix | fixed hashed_password type to String(255)

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy.sql import func
 
@@ -8,8 +8,8 @@ class User(Base):
   __tablename__ = "users"
   id = Column(Integer, primary_key=True)
   email = Column(String, unique=True, nullable=False)
-  hashed_password = Column(String, nullable=False)
-  created_at = Column(Date, nullable=False)
+  hashed_password = Column(String(255), nullable=False)
+  created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
 
   refresh_tokens = relationship('RefreshToken', back_populates='user', passive_deletes=True)
 
@@ -18,8 +18,8 @@ class RefreshToken(Base):
   id = Column(Integer, primary_key=True)
   hashed_token = Column(String, unique=True, nullable=False)
   user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
-  expires_at = Column(Date, nullable=False)
-  created_at = Column(Date, nullable=False)
+  expires_at = Column(DateTime(timezone=True), nullable=False)
+  created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
   user_agent = Column(String)
   ip_address = Column(String)
   
