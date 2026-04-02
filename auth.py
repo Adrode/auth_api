ef6c96cbbdda55fb8dc1dc954cc4acebba9af123
Adrian Wozniak <adrianwozniak20@gmail.com>
2026-04-02 15:48:28 +0200

    feature | create_access_token for logging, secret_key hid

--- auth.py
+++ auth.py
@@ -1,4 +1,14 @@
+from dotenv import load_dotenv
+import os
 from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
+
+TIME_TO_EXPIRE = 15
 
 pwd_context = CryptContext(schemes=['bcrypt'])
 
@@ -6,6 +16,13 @@ print("schemes: ", pwd_context.schemes())
 print("default scheme: ", pwd_context.default_scheme())
 
 def hash_password(password: str) -> str:
-  hashed = pwd_context.hash(password)
-  print(f"plain: {password}, hashed: {hashed}")
-  return hashed
+  return pwd_context.hash(password)
+
+def verify_password(plain: str, hashed: str) -> bool:
+  return pwd_context.verify(plain, hashed)
+
+def create_access_token(data: dict) -> str:
+  to_encode = data.copy()
+  exp = datetime.now() + timedelta(minutes=TIME_TO_EXPIRE)
+  to_encode.update({'exp': exp})
+  return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
