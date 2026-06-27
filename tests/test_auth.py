2f43e0de4a8ca14814552c1323f5c405cc7123b0
Adrian Wozniak <adrianwozniak20@gmail.com>
2026-06-27 13:03:13 +0200

    feature: logout endpoint tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -136,3 +136,32 @@ def test_refresh_user_agent_mismatch(client, test_first_user):
 
   assert response.status_code == 200
   assert response.json()["warnings"]["user-agent"] == "Suspicious device"
+
+def test_logout_valid_data(client, test_first_user):
+  login = client.post(
+    "/auth/login",
+    data={
+      "username": test_first_user.email,
+      "password": test_first_user.plain_password
+    }
+  )
+
+  response = client.post(
+    "/auth/logout",
+    json={
+      "refresh_token": login.json()["refresh_token"]
+    }
+  )
+
+  assert response.status_code == 200
+  assert response.json()["detail"] == "Logged out"
+
+def test_logout_invalid_old_token(client):
+  response = client.post(
+    "/auth/logout",
+    json={
+      "refresh_token": "kekw"
+    }
+  )
+
+  assert response.status_code == 401
